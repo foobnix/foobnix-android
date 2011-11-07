259fde546790c40dda530c5d45c58a57772c7c01
Ivan Ivanenko <iivanenko@congiance.com>
2011-11-07 20:25:46 +0200

    make more modules
    Change-Id: I2521fd07201053c163211b21d48692b727752c26

---
+++ foobnix-parent-android/foobnix-android-widgets/src/main/java/com/foobnix/android/widgets/navlist/NavigationActivity.java
@@ -0,0 +1,14 @@
+package com.foobnix.android.widgets.navlist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NavigationActivity  extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+
+}
---
+++ foobnix-parent-android/foobnix-android-widgets/src/main/java/com/foobnix/android/widgets/navlist/NavigationList.java
@@ -0,0 +1,8 @@
+package com.foobnix.android.widgets.navlist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NavigationList{
+	
+}
