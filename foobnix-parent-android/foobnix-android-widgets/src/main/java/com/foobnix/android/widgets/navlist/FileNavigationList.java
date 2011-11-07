1ffbf5925c916d80e1bab376f3c9fd6856b6a315
Ivan Ivanenko <iivanenko@congiance.com>
2011-11-07 20:34:32 +0200

    changes
    Change-Id: I38f25e75b8a66ffbc21400512fcbffc8ce704b91

---
+++ foobnix-parent-android/foobnix-android-widgets/src/main/java/com/foobnix/android/widgets/navlist/FileNavigationList.java
@@ -0,0 +1,13 @@
+package com.foobnix.android.widgets.navlist;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+
+public class FileNavigationList extends LinearLayout{
+	
+	public FileNavigationList(Context context) {
+		super(context);
+	}
+	
+}
--- foobnix-parent-android/foobnix-android-widgets/src/main/java/com/foobnix/android/widgets/navlist/NavigationList.java
+++
@@ -1,8 +0,0 @@
-package com.foobnix.android.widgets.navlist;
-
-import android.app.Activity;
-import android.os.Bundle;
-
-public class NavigationList{
-	
-}
